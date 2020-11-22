13aac084ea116dd7ee19515db0103a3a05b1cec9
Albert Szeliga <albert.szeliga@stud.hs-hannover.de>
2020-11-22 18:00:08 +0100

    added window swap functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,50 @@
+import { GUI } from "../../libJs/dat.gui.module.js";
 import { WebGLRenderer } from "../libJs/three.module.js";
 import { BezierWindow } from "./windows/bezierWindow.js";
 import { QuaternionWindow } from "./windows/quaternionWindow.js";
 
-var mainWindow = new BezierWindow();
-mainWindow.hideGUI();
-var mainWindow = new QuaternionWindow();
+var windowInstances = [
+	new BezierWindow(),
+	new QuaternionWindow()
+]
+
+var windowIndecies = {
+	bezier: 0,
+	quaternion: 1
+};
+
+var globalOptions = {
+	window: 0
+};
+
+for(let i = 0; i < windowInstances.length; i++) {
+	windowInstances[i].getGUI().hide();
+}
+
+var currentWindow = windowInstances[globalOptions.window];
+currentWindow.getGUI().show();
+
+var windowsSelectGui = new GUI();
+
+var windowFolder = windowsSelectGui.addFolder("Current window");
+windowFolder.add(globalOptions, "window", windowIndecies).onChange((newWindowIndex) => {
+	currentWindow.getGUI().hide();
+
+	currentWindow = windowInstances[newWindowIndex];
+
+	currentWindow.getGUI().show();
+});
+windowFolder.open();
 
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 function animate(time) {
-  mainWindow.update(time);
+	currentWindow.update(time);
 
-  renderer.render(mainWindow.getScene(), mainWindow.getCamera());
+	renderer.render(currentWindow.getScene(), currentWindow.getCamera());
 
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
