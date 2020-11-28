bf9608f1546cdb572e00ad3e41005ad32c72e7db
Ture <ture.cl.business@gmail.com>
2020-11-28 11:28:38 +0100

    Move window control to left

--- js/main.js
+++ js/main.js
@@ -1,53 +1,57 @@
-import * as THREE from "/three/three.module.js";
-import * as DAT from "/dat/dat.gui.module.js";
-
 //import { GUI } from "../../libJs/dat.gui.module.js";
 //import { TetrahedronGeometry, WebGLRenderer } from "../libJs/three.module.js";
 import { BezierWindow } from "./windows/bezierWindow/bezierWindow.js";
 import { QuaternionWindow } from "./windows/quaternionWindow/quaternionWindow.js";
+import * as DAT from "/dat/dat.gui.module.js";
+import * as THREE from "/three/three.module.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var windowInstances = [
-	new BezierWindow(renderer),
-	new QuaternionWindow(renderer)
-]
+  new BezierWindow(renderer),
+  new QuaternionWindow(renderer),
+];
 
 var windowIndecies = {
-	bezier: 0,
-	quaternion: 1
+  bezier: 0,
+  quaternion: 1,
 };
 
 var globalOptions = {
-	window: 0
+  window: 0,
 };
 
-for(let i = 0; i < windowInstances.length; i++) {
-	windowInstances[i].getGUI().hide();
+for (let i = 0; i < windowInstances.length; i++) {
+  windowInstances[i].getGUI().hide();
 }
 
 var currentWindow = windowInstances[globalOptions.window];
 currentWindow.getGUI().show();
 
-var windowsSelectGui = new DAT.GUI();
+var windowsSelectGui = new DAT.GUI({ autoPlace: false });
 
 var windowFolder = windowsSelectGui.addFolder("Current window");
-windowFolder.add(globalOptions, "window", windowIndecies).onChange((newWindowIndex) => {
-	currentWindow.getGUI().hide();
+windowFolder
+  .add(globalOptions, "window", windowIndecies)
+  .onChange((newWindowIndex) => {
+    currentWindow.getGUI().hide();
 
-	currentWindow = windowInstances[newWindowIndex];
+    currentWindow = windowInstances[newWindowIndex];
 
-	currentWindow.getGUI().show();
-});
+    currentWindow.getGUI().show();
+  });
 windowFolder.open();
+document
+  .getElementById("window-control")
+  .appendChild(windowsSelectGui.domElement);
 
 function animate(time) {
-	currentWindow.update(time);
+  currentWindow.update(time);
 
-	renderer.render(currentWindow.getScene(), currentWindow.getCamera());
+  renderer.render(currentWindow.getScene(), currentWindow.getCamera());
 
-	requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
