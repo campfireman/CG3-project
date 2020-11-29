d3d2479ebf4abb72ccf62f3d5e04bbfc3ddfc39e
Ture <ture.cl.business@gmail.com>
2020-11-29 01:09:49 +0100

    Add basic controls

--- js/windows/quaternionWindow/Quaternion.js
+++ js/windows/quaternionWindow/Quaternion.js
@@ -1,11 +1,121 @@
+import { Matrix4, Quaternion, Vector3 } from "../../../libJs/three.module.js";
 class QuaternionCustom {
-  constructor(vector, theta) {
-    this.vector = vector;
-    this.theta = theta;
-    this.x = this.vector.x;
-    this.y = this.vector.y;
-    this.z = this.vector.z;
+  constructor(s, x, y, z) {
+    this.s = s;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.matrix = new Matrix4();
+    this.matrix.set(
+      1 - 2 * (Math.pow(this.y, 2) + Math.pow(this.z, 2)),
+      2 * (this.x * this.y - this.s * this.z),
+      2 * (this.x * this.z + this.s * this.y),
+      0,
+      2 * (this.x * this.y + this.s * this.z),
+      1 - 2 * (Math.pow(this.x, 2) + Math.pow(this.z, 2)),
+      2 * (this.y * this.z - this.s * this.x),
+      0,
+      2 * (this.x * this.z - this.s * this.y),
+      2 * (this.y * this.z + this.s * this.x),
+      1 - 2 * (Math.pow(this.x, 2) + Math.pow(this.y, 2)),
+      0,
+      0,
+      0,
+      0,
+      1
+    );
+  }
+  normalize() {
+    return this.multiplyScalar(1 / this.norm());
+  }
+  // operators
+  add(other) {
+    return new QuaternionCustom(
+      this.s + other.s,
+      this.x + other.x,
+      this.y + other.y,
+      this.z + other.z
+    );
+  }
+  dot(other) {
+    return (
+      this.s * other.s + this.x * other.x + this.y * other.y + this.z * other.z
+    );
+  }
+  multiply(other) {
+    let s1 = this.s;
+    let s2 = other.s;
+    let v1 = new Vector3(this.x, this.y, this.z);
+    let v2 = new Vector3(other.x, other.y, other.z);
+    let res = v2
+      .clone()
+      .multiplyScalar(s1)
+      .add(v1.clone().multiplyScalar(s2))
+      .add(v1.clone().cross(v2));
+    return new QuaternionCustom(s1 * s2 - v1.dot(v2), res.x, res.y, res.z);
+  }
+  multiplyScalar(scalar) {
+    return new QuaternionCustom(
+      this.s * scalar,
+      this.x * scalar,
+      this.y * scalar,
+      this.z * scalar
+    );
+  }
+  norm() {
+    let res = new Vector3(this.x, this.y, this.z);
+    return Math.sqrt(Math.pow(this.s, 2) + res.dot(res.clone()));
+  }
+  invert() {
+    return new Quaternion(this.s, -this.x, -this.y, -this.z).multiplyScalar(
+      1 / Math.pow(this.norm, 2)
+    );
+  }
+  negate() {
+    return new Quaternion(-this.s, -this.x, -this.y, -this.z);
+  }
+
+  slerp(other, t) {
+    let v0 = this.normalize();
+    let v1 = other.normalize();
+
+    let dot = v0.dot(v1);
+    if (dot < 0.0) {
+      v1 = v1.negate();
+      dot = -dot;
+    }
+    if (dot > 0.9995) {
+      let result = v0.add(v1.add(v0.negate()).multiplyScalar(t));
+      return result.normalize();
+    }
+    let theta_0 = Math.acos(dot);
+    let theta = theta_0 * t;
+    let sin_theta = Math.sin(theta);
+    let sin_theta_0 = Math.sin(theta_0);
+    let s0 = Math.cos(theta) - (dot * sin_theta) / sin_theta_0;
+    let s1 = sin_theta / sin_theta_0;
+    return v0.multiplyScalar(s0).add(v1.multiplyScalar(s1));
   }
 }
 
-export { QuaternionCustom };
+class QuaternionAngle extends QuaternionCustom {
+  constructor(theta, x, y, z) {
+    let s = Math.cos(theta / 2);
+    let angle = Math.sin(theta / 2);
+    let v_x = x * angle;
+    let v_y = y * angle;
+    let v_z = z * angle;
+    super(s, v_x, v_y, v_z);
+    this.theta = theta;
+    this.a = new Vector3(x, y, z);
+  }
+  updateValues() {
+    this.s = Math.cos(this.theta / 2);
+    let angle = Math.sin(this.theta / 2);
+    this.x = this.a.x * angle;
+    this.y = this.a.y * angle;
+    this.z = this.a.z * angle;
+  }
+}
+
+export { QuaternionCustom, QuaternionAngle };
--- js/windows/quaternionWindow/quaternionWindow.js
+++ js/windows/quaternionWindow/quaternionWindow.js
@@ -5,33 +5,35 @@ import {
   GridHelper,
   PerspectiveCamera,
   Scene,
-  Vector3,
 } from "../../../libJs/three.module.js";
 import { Window } from "../window.js";
-import { QuaternionCustom as Quaternion } from "./Quaternion.js";
+import { QuaternionAngle } from "./Quaternion.js";
 import { OrbitControls } from "/jsm/controls/OrbitControls.js";
 
 const MODEL_PATH = "../../../models/";
 
 const MIN_AXIS_VALUE = -10.0;
 const MIN_THETA_VALUE = 0.0;
+const MIN_ANIMATION_TIME = 100;
 
 const MAX_AXIS_VALUE = 10.0;
-const MAX_THETA_VALUE = 2.0;
+const MAX_THETA_VALUE = 2.0 * Math.PI;
+const MAX_ANIMATION_TIME = 5000;
 
 const DEFAULT_X_VALUE = 1.0;
-const DEFAULT_Y_VALUE = 1.0;
-const DEFAULT_Z_VALUE = 1.0;
-const DEFAULT_THETA_VALUE = 0.5;
+const DEFAULT_Y_VALUE = 0.0;
+const DEFAULT_Z_VALUE = 0.0;
+const DEFAULT_THETA_VALUE = 0.5 * Math.PI;
+const DEFAULT_ANIMATION_TIME = 2000;
 
 class QuaternionWindow extends Window {
   constructor(renderer) {
     super(renderer);
     this.count = 0;
+    this.quaternions = [];
     this.options = {
-      velx: 0,
-      vely: 0,
-      count: 0,
+      animate: false,
+      animationTime: DEFAULT_ANIMATION_TIME,
       camera: {
         speed: 0.0001,
       },
@@ -39,58 +41,10 @@ class QuaternionWindow extends Window {
         if (this.count > 0) {
           this.quaternions[this.count - 1].folder.close();
         }
-        let newQuaternionFolder = this.quaternionFolder.addFolder(
-          `Quaternion ${this.count}`
-        );
-        let attributes = {
-          folder: newQuaternionFolder,
-          quaternion: new Quaternion(
-            new Vector3(DEFAULT_X_VALUE, DEFAULT_Y_VALUE, DEFAULT_Z_VALUE),
-            DEFAULT_THETA_VALUE
-          ),
-        };
-        this.quaternions[this.count] = attributes;
-        newQuaternionFolder
-          .add(
-            this.quaternions[this.count].quaternion,
-            "x",
-            MIN_AXIS_VALUE,
-            MAX_AXIS_VALUE
-          )
-          .name("X")
-          .listen();
-        newQuaternionFolder
-          .add(
-            this.quaternions[this.count].quaternion,
-            "y",
-            MIN_AXIS_VALUE,
-            MAX_AXIS_VALUE
-          )
-          .name("Y")
-          .listen();
-        newQuaternionFolder
-          .add(
-            this.quaternions[this.count].quaternion,
-            "z",
-            MIN_AXIS_VALUE,
-            MAX_AXIS_VALUE
-          )
-          .name("Z")
-          .listen();
-        newQuaternionFolder
-          .add(
-            this.quaternions[this.count].quaternion,
-            "theta",
-            MIN_THETA_VALUE,
-            MAX_THETA_VALUE
-          )
-          .name("Theta")
-          .listen();
-        newQuaternionFolder.open();
-        this.count++;
+        this.addQuaternion();
       },
       removeQuaternion: () => {
-        if (this.count == 0) {
+        if (this.count == 1) {
           return;
         }
         this.count--;
@@ -99,6 +53,9 @@ class QuaternionWindow extends Window {
         if (this.count > 0) {
           this.quaternions[this.count - 1].folder.open();
         }
+      },
+      startAnimation: () => {
+        this.options.animate = !this.options.animate;
       },
     };
 
@@ -142,7 +99,7 @@ class QuaternionWindow extends Window {
     this.gui = new GUI();
 
     this.quaternionFolder = this.gui.addFolder("Quaternions");
-    this.quaternions = {};
+    this.quaternionFolder.open();
 
     this.quaternionFolder
       .add(this.options, "addQuaternion")
@@ -150,14 +107,118 @@ class QuaternionWindow extends Window {
     this.quaternionFolder
       .add(this.options, "removeQuaternion")
       .name("Remove quaternion");
-    this.quaternionFolder.open();
-  }
 
+    this.addQuaternion();
+    this.animationFolder = this.gui.addFolder("Animation");
+    this.animationFolder.add(this.options, "startAnimation");
+    this.animationFolder
+      .add(
+        this.options,
+        "animationTime",
+        MIN_ANIMATION_TIME,
+        MAX_ANIMATION_TIME
+      )
+      .name("Animation time")
+      .listen();
+    this.animationFolder.open();
+
+    // animation
+    this.resetAnimation();
+  }
+  resetAnimation() {
+    this.sum = 0;
+    this.prevQ = new QuaternionAngle(0, 1, 0, 0);
+    this.curQ = this.quaternions[0].quaternion;
+    this.prevT = 0;
+    this.first = true;
+    this.cur = 1;
+  }
+  updateQuaternions() {
+    this.quaternions.forEach((val) => {
+      val.quaternion.updateValues();
+    });
+  }
   update(time) {
-    if (this.object != null) {
+    if (this.object != null && this.options.animate) {
+      if (this.first) {
+        this.prevT = time;
+        this.first = false;
+        this.updateQuaternions();
+      }
+      let delta = time - this.prevT;
+      let t = this.sum / (this.options.animationTime / this.quaternions.length);
+      this.prevT = time;
+      this.sum += delta;
+      if (this.sum > this.options.animationTime) {
+        this.resetAnimation();
+        return;
+      }
+      if (
+        this.sum >
+        this.cur * (this.options.animationTime / this.quaternions.length)
+      ) {
+        this.prevQ = this.curQ;
+        this.curQ = this.quaternions[this.cur].quaternion;
+        this.cur++;
+      }
+      let pos = this.prevQ.slerp(this.curQ, t);
+      this.object.setRotationFromMatrix(pos.matrix);
     }
   }
-
+  addQuaternion() {
+    let newQuaternionFolder = this.quaternionFolder.addFolder(
+      `Quaternion ${this.count}`
+    );
+    let attributes = {
+      folder: newQuaternionFolder,
+      quaternion: new QuaternionAngle(
+        DEFAULT_THETA_VALUE + this.count * DEFAULT_THETA_VALUE,
+        DEFAULT_X_VALUE +
+          this.count * DEFAULT_X_VALUE * ((this.count % 2) * -1),
+        DEFAULT_Y_VALUE,
+        DEFAULT_Z_VALUE
+      ),
+    };
+    this.quaternions[this.count] = attributes;
+    newQuaternionFolder
+      .add(
+        this.quaternions[this.count].quaternion.a,
+        "x",
+        MIN_AXIS_VALUE,
+        MAX_AXIS_VALUE
+      )
+      .name("X")
+      .listen();
+    newQuaternionFolder
+      .add(
+        this.quaternions[this.count].quaternion.a,
+        "y",
+        MIN_AXIS_VALUE,
+        MAX_AXIS_VALUE
+      )
+      .name("Y")
+      .listen();
+    newQuaternionFolder
+      .add(
+        this.quaternions[this.count].quaternion.a,
+        "z",
+        MIN_AXIS_VALUE,
+        MAX_AXIS_VALUE
+      )
+      .name("Z")
+      .listen();
+    newQuaternionFolder
+      .add(
+        this.quaternions[this.count].quaternion,
+        "theta",
+        MIN_THETA_VALUE,
+        MAX_THETA_VALUE
+      )
+      .name("Theta")
+      .listen();
+    newQuaternionFolder.open();
+    this.count++;
+  }
   getScene() {
     return this.scene;
   }
