eddfe7cb1557913133c4777a2f3408b2fd23f9b3
Ture <ture.cl.business@gmail.com>
2020-12-20 04:36:58 +0100

    Integrator

--- js/windows/clothWindow/Intergrators.js
+++ js/windows/clothWindow/Intergrators.js
@@ -8,19 +8,16 @@ function integrateEuler(xOrg, dxOrg, dt) {
 }
 
 function integrateRungeKutta(xOrg, dxOrg, h) {
-    // let x = xOrg.clone();
-    // let dx = dxOrg.clone();
-
-    // let k_1 = 
-    // let k_2 = 
-    // let k_3 = 
-    // let k_4 = 
-
-    // return x + k_1.multiplyScalar(h / 6) + k_2.multiplyScalar(h / 3) + k_3.multiplyScalar(h / 3) + k_4.multiplyScalar(h / 6)
+    // work in progress
     let x = xOrg.clone();
     let dx = dxOrg.clone();
+    let h_2 = h/2;
+    let k_1 = x
+    let k_2 = x.clone().add(k_1.clone().multiplyScalar(h_2))
+    let k_3 = x.clone().add(k_2.clone().multiplyScalar(h_2))
+    let k_4 = x.clone().add(k_2.clone().multiplyScalar(h))
 
-    return x.add(dx.multiplyScalar(dt));
+    return x.clone().add(k_1.multiplyScalar(h / 6).add(k_2.multiplyScalar(h / 3).add(k_3.multiplyScalar(h / 3).add(k_4.multiplyScalar(h / 6)))))
 }
 
 export { integrateEuler, integrateRungeKutta };
